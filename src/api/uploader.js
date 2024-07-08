eef10314710755181aff2bd7c15d89bbc2104d16
minji7901 <88637782+minji7901@users.noreply.github.com>
2024-07-08 21:19:58 +0900

    2024-07-08 compeleted

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -5,6 +5,7 @@ export async function uploadImage(file) {
    // FormData에 파일과 업로드 프리셋 추가
   data.append('file', file);
   data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_PRESET);
+  data.append('folder', 'mjnogi');
   
   return fetch(process.env.REACT_APP_CLOUDINARY_URL, {
     method: 'POST',
